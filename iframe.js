198b39e96860fa11c6781b66229ad7932a455f54
Shivraj Chvada <shivraj@webtheunivesre.com>
2022-12-12 19:08:53 +0530

    style added

--- iframe.js
+++ iframe.js
@@ -1,12 +1,12 @@
 const ssBot = document.createElement('div');
-ssBot.id = "socialsparsh-bot"; ssBot.style.position = "fixed"; ssBot.style.bottom = "70px"; ssBot.style.right = "70px"; ssBot.style.display = "flex"; ssBot.style.flexDirection = "column-reverse"; ssBot.style.alignItems = "flex-end"
+ssBot.id = "socialsparsh-bot"; ssBot.style.position = "fixed"; ssBot.style.bottom = "70px"; ssBot.style.right = "70px"; ssBot.style.display = "flex"; ssBot.style.flexDirection = "column-reverse"; ssBot.style.alignItems = "flex-end"; ssBot.style.zIndex = 11111;
 const ssbFrame = document.createElement("iframe");
 ssbFrame.id = "ssb-frame"; ssbFrame.height = "500px"; ssbFrame.width = "300px"; ssbFrame.style.border = "0px"; ssbFrame.style.borderRadius = "10px"; ssbFrame.style.boxShadow = "rgba(149, 157, 165, 0.2) 0px 8px 24px"; ssbFrame.style.display = "none";
 ssBot.append(ssbFrame);
 const button = document.createElement('button');
 button.innerHTML = "open Bot";
 ssBot.append(button);
-button.style.border = "1px solid black"; button.style.padding = "5px";
+button.style.border = "1px solid black"; button.style.padding = "5px"; button.style.outline = "none";
 let isOpen = false;
 button.onclick = () => {
     ssbFrame.style.display = isOpen ? "none" : "block"
